af6f42c53401c47775fdb55ccbac4f32e5671c17
hitesh-bst <59194184+hitesh-bst@users.noreply.github.com>
2020-04-19 10:03:17 +0530

    Updates

---
+++ precache-manifest.39dd1c0e30b7b55672f1cb9983c159d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a742a5349d416de2ac501d79f7de1ca",
+    "url": "/next-break/index.html"
+  },
+  {
+    "revision": "0c2ba0a134bb8a2c4570",
+    "url": "/next-break/static/css/main.39cb48c5.chunk.css"
+  },
+  {
+    "revision": "11b1fe7a9c3676781a18",
+    "url": "/next-break/static/js/2.cc4f3124.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/next-break/static/js/2.cc4f3124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2ba0a134bb8a2c4570",
+    "url": "/next-break/static/js/main.e73dff11.chunk.js"
+  },
+  {
+    "revision": "e17fcd034e548e396d0c",
+    "url": "/next-break/static/js/runtime-main.f37a963b.js"
+  }
+]);
