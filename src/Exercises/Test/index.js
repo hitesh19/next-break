1f1406d01d5be57ecd84e01fe4b0ddbeea8edcc6
Guinnes Singh <gswebspace@gmail.com>
2020-11-17 21:19:31 +0530

    WIP: posenet output visualized (for testing)

--- src/Exercises/Test/index.js
+++ src/Exercises/Test/index.js
@@ -6,6 +6,8 @@ import * as tf from '@tensorflow/tfjs';
 
 
 const DURATION = 60000;
+const MINIMUM_POSE_SCORE = 0.1;
+const COLOR = `aqua`;
 
 class Test extends React.Component {
 
@@ -17,7 +19,8 @@ class Test extends React.Component {
         resolution: {
             width: 500,
             height: 600
-        }
+        },
+        showVideo: false
     };
 
     async componentDidMount() {
@@ -25,8 +28,11 @@ class Test extends React.Component {
 
         //Initialize camera
         let video = await this.setupCamera(this.state.resolution.width, this.state.resolution.height);
-        let timerId = setInterval(process, 1000);
+        let timerId = setInterval(this.process.bind(this), 1000);
 
+        //Initialize Canvas context for drawing
+        const canvas = document.getElementById('output');
+        const ctx = canvas.getContext('2d');
 
         //Initialize posenet
         const net = await posenet.load({
@@ -40,7 +46,10 @@ class Test extends React.Component {
         this.setState({
             timerId: timerId,
             video: video,
-            net: net
+            net: net,
+            showVideo: true,
+            canvas: canvas,
+            ctx: ctx
         })
 
     }
@@ -74,7 +83,65 @@ class Test extends React.Component {
     }
 
     async process() {
-        console.log(this.state.video, this.state.net);
+        // console.log(this.state.video, this.state.net);
+
+        //Draw latest captured image
+        this.state.ctx.clearRect(0, 0, this.state.resolution.width, this.state.resolution.height);
+        this.state.ctx.save();
+        this.state.ctx.scale(-1, 1);
+        this.state.ctx.translate(-this.state.resolution.width, 0);
+        this.state.ctx.drawImage(this.state.video, 0, 0, this.state.resolution.width, this.state.resolution.height);
+        this.state.ctx.restore();
+
+        //If posenet is available, perform pose estimation
+        if (this.state.net) {
+            const poses = await this.state.net.estimatePoses(this.state.video, {
+                flipHorizontal: true,
+                decodingMethod: 'single-person'
+            });
+
+            if (poses && poses.length > 0) {
+                let keypoints = poses[0].keypoints;
+                let score = poses[0].score;
+                console.log("Overall Score = ", score);
+                if (score >= MINIMUM_POSE_SCORE) {
+                    this.drawKeypoints(keypoints, MINIMUM_POSE_SCORE, this.state.ctx, 1, "nose");
+                    // drawSkeleton(keypoints, minPartConfidence, ctx);
+                    // drawBoundingBox(keypoints, ctx);
+
+                }
+            }
+
+
+
+            //   poses = poses.concat(pose);
+        }
+
+    }
+
+    drawKeypoints(keypoints, minConfidence, ctx, scale = 1, keypointToShow) {
+        for (let i = 0; i < keypoints.length; i++) {
+            const keypoint = keypoints[i];
+
+            if(keypointToShow){
+                if (keypoint.part !== keypointToShow){
+                    continue;
+                }
+            }
+            if (keypoint.score < minConfidence) {
+                continue;
+            }
+            console.log("Nose Score=", keypoint.score)
+            const { y, x } = keypoint.position;
+            this.drawPoint(ctx, y * scale, x * scale, 3, COLOR);
+        }
+    }
+
+    drawPoint(ctx, y, x, r, color) {
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, 2 * Math.PI);
+        ctx.fillStyle = color;
+        ctx.fill();
     }
 
     // async updateProgress() {
@@ -123,9 +190,9 @@ class Test extends React.Component {
         return (
             <div>
                 <div>Test exercise</div>
-                <video id="video" playsInline style={{"display" : "none"}}>
+                <video id="video" playsInline style={{ "display": "none" }} >
                 </video>
-                <canvas id="output" />
+                <canvas width={this.state.resolution.width} height={this.state.resolution.height} id="output" />
             </div>
 
         )
